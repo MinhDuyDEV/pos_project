bfdac7b01a9c77ced9da1f5347c588750ea4a9c4
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-12-10 14:42:53 +0700

    update code

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ["source.unsplash.com", "s.gravatar.com", "res.cloudinary.com"],
   },
   env: {
-    NEXT_PUBLIC_BASE_URL: "https://pos-project-server-minhduydev.vercel.app/",
+    NEXT_PUBLIC_BASE_URL: "https://pos-project-server-minhduydev.vercel.app",
   },
 };
 
